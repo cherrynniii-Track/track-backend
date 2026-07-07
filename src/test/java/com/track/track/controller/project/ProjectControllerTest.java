6ba8be3547bc665e2145494f4fd572efb68dc2aa
cherrynniii <chaelin20760@gmail.com>
2026-07-08 01:07:25 +0900

    test: DB 연결 안 되는 원인 찾기

--- src/test/java/com/track/track/controller/project/ProjectControllerTest.java
+++ src/test/java/com/track/track/controller/project/ProjectControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
@@ -20,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * ProjectController 통합 테스트
  */
-@Transactional
 class ProjectControllerTest extends AbstractIntegrationTest {
 
     @Autowired
